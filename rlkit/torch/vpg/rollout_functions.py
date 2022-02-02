9ede900125286b22c31bb9089fe4c7f00391bf97
Tanmay Kulkarni <tanmaygk97@gmail.com>
2022-02-02 07:05:35 -0500

    add basic template for rollouts

--- rlkit/torch/vpg/rollout_functions.py
+++ rlkit/torch/vpg/rollout_functions.py
@@ -1,3 +1,5 @@
+import copy
+
 def vec_rollout(
     env,
     agent,
@@ -14,10 +16,49 @@ def vec_rollout(
     rewards = [np.zeros(num_envs)]
     actions = [np.zeros((num_envs, env_actions_space.low.size))]
     terminals = [[False] * num_envs]
-    agent_info = [{}]
-    env_info = [{}]
+    agent_infos = [{}]
+    env_infos = [{}]
 
     for step in range(0, max_path_length):
         action, agent_info = agent.get_action(policy_obs)
 
-        obs, reward, done, info = env.step()
+        obs, reward, done, info = env.step(copy.deepcopy(action))
+        
+        observations.append(obs)
+        rewards.append(reward)
+        terminals.append(done)
+        actions.append(action)
+        agent_infos.append(agent_info)
+        env_infos.append(info)
+
+        if done.all():
+            break
+        
+        policy_obs = obs
+
+    actions = np.array(actions)
+
+    if len(actions.shape) == 1:
+        actoins = np.expand_dims(actions, 1)
+    
+    env_info_final = {}
+
+    for info in env_infos[1:]:
+        for key, vlaue in info.items():
+            if key not in env_info_final:
+                env_info_final[key] = []
+            
+            env_info_final[key].append(value)
+    
+    for key, value in env_info_final.items():
+        env_info_final[key] = np.concatenate(value, 1)
+    env_infos = env_info_final
+
+    return dict(
+        observations=observations,
+        actions=actions,
+        rewards=rewards,
+        terminals=np.array(terminals),
+        agent_infos=agent_infos,
+        env_infos=env_infos
+    )
