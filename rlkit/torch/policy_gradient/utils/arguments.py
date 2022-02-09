38cbd22a58d4460db9806646b16bd2a29b2d4751
Tanmay Kulkarni <tanmaygk97@gmail.com>
2022-02-09 07:02:05 -0500

    add parser for command line arguments to launch experiments

---
+++ rlkit/torch/policy_gradient/utils/arguments.py
@@ -0,0 +1,42 @@
+import argparse
+
+def convert_value_to_type(value, type_):
+    if type_ == "bool":
+        value = value == "True"
+    elif type_ == "int":
+        value = int(value)
+    elif type_ == "float":
+        value = float(value)
+    elif type_ == "str":
+        value = value
+    else:
+        raise NotImplementedError(f'type {type_} is not implemented')
+    
+    return value
+
+def get_args():
+    parser = argparse.ArgumentParser("Arguments for Experiment")
+    parser.add_argument("--exp_prefix", type=str, default="test")
+    parser.add_argument("--num_seeds", type=int, default=1)
+    parser.add_argument("--mode", type=str, default="local")
+    parser.add_argument("--debug", action="store_true", default=False)
+    parser.add_argument("-sk", "--search_keys", type=str, nargs='*', default=[])
+    parser.add_argument("-sv", "--search_values", nargs='*', default=[])
+    
+    args = parser.parse_args()
+
+    updated_search_values = []
+    for value, type_ in zip(args.search_values, args.search_types):
+        if type(value) == list:
+            updated_value = []
+
+            for nested_value in value:
+                nested_value = convert_value_to_type(nested_value, type_)
+                updated_value.append(nested_value)
+            
+        else:
+            updated_value = convert_value_to_type(value, type_)
+        
+        updated_search_values.append(updated_value)
+    
+    args.search_value = updated_search_values
