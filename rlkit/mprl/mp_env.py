3f9347b781e2128c60e6956e1b4afbb9949a7442
Murtaza Dalal <mdalal@andrew.cmu.edu>
2022-05-06 14:07:52 -0400

    fixed bugs in rotation handling

--- rlkit/mprl/mp_env.py
+++ rlkit/mprl/mp_env.py
@@ -8,18 +8,6 @@ from rlkit.envs.proxy_env import ProxyEnv
 import matplotlib
 
 try:
-    # graph-tool and py-OMPL have some minor issues coexisting with each other.  Both modules
-    # define conversions to C++ STL containers (i.e. std::vector), and the module that is imported
-    # first will have its conversions used.  Order doesn't seem to matter on Linux,
-    # but on Apple, graph_tool will not be imported properly if OMPL comes first.
-    import graph_tool.all as gt
-
-    graphtool = True
-except ImportError:
-    print("Failed to import graph-tool.  PlannerData will not be analyzed or plotted")
-    graphtool = False
-
-try:
     from ompl import base as ob
     from ompl import util as ou
     from ompl import geometric as og
@@ -192,101 +180,6 @@ def apply_controller(controller, action, robot, policy_step):
 
     # Apply joint torque control
     robot.sim.data.ctrl[robot._ref_joint_actuator_indexes] = torques
-
-
-def useGraphTool(pd):
-    # Extract the graphml representation of the planner data
-    graphml = pd.printGraphML()
-    f = open("graph.graphml", "w")
-    f.write(graphml)
-    f.close()
-
-    # Load the graphml data using graph-tool
-    graph = gt.load_graph("graph.graphml", fmt="xml")
-    edgeweights = graph.edge_properties["weight"]
-
-    # Write some interesting statistics
-    avgdeg, stddevdeg = gt.vertex_average(graph, "total")
-    avgwt, stddevwt = gt.edge_average(graph, edgeweights)
-
-    print("---- PLANNER DATA STATISTICS ----")
-    print(
-        str(graph.num_vertices()) + " vertices and " + str(graph.num_edges()) + " edges"
-    )
-    print(
-        "Average vertex degree (in+out) = "
-        + str(avgdeg)
-        + "  St. Dev = "
-        + str(stddevdeg)
-    )
-    print("Average edge weight = " + str(avgwt) + "  St. Dev = " + str(stddevwt))
-
-    _, hist = gt.label_components(graph)
-    print("Strongly connected components: " + str(len(hist)))
-
-    # Make the graph undirected (for weak components, and a simpler drawing)
-    graph.set_directed(False)
-    _, hist = gt.label_components(graph)
-    print("Weakly connected components: " + str(len(hist)))
-
-    # Plotting the graph
-    gt.remove_parallel_edges(graph)  # Removing any superfluous edges
-
-    edgeweights = graph.edge_properties["weight"]
-    colorprops = graph.new_vertex_property("string")
-    vertexsize = graph.new_vertex_property("double")
-
-    start = -1
-    goal = -1
-
-    for v in range(graph.num_vertices()):
-
-        # Color and size vertices by type: start, goal, other
-        if pd.isStartVertex(v):
-            start = v
-            colorprops[graph.vertex(v)] = "cyan"
-            vertexsize[graph.vertex(v)] = 10
-        elif pd.isGoalVertex(v):
-            goal = v
-            colorprops[graph.vertex(v)] = "green"
-            vertexsize[graph.vertex(v)] = 10
-        else:
-            colorprops[graph.vertex(v)] = "yellow"
-            vertexsize[graph.vertex(v)] = 5
-
-    # default edge color is black with size 0.5:
-    edgecolor = graph.new_edge_property("string")
-    edgesize = graph.new_edge_property("double")
-    for e in graph.edges():
-        edgecolor[e] = "black"
-        edgesize[e] = 0.5
-
-    # using A* to find shortest path in planner data
-    if start != -1 and goal != -1:
-        _, pred = gt.astar_search(graph, graph.vertex(start), edgeweights)
-
-        # Color edges along shortest path red with size 3.0
-        v = graph.vertex(goal)
-        while v != graph.vertex(start):
-            p = graph.vertex(pred[v])
-            for e in p.out_edges():
-                if e.target() == v:
-                    edgecolor[e] = "red"
-                    edgesize[e] = 2.0
-            v = p
-
-    # Writing graph to file:
-    # pos indicates the desired vertex positions, and pin=True says that we
-    # really REALLY want the vertices at those positions
-    gt.graph_draw(
-        graph,
-        vertex_size=vertexsize,
-        vertex_fill_color=colorprops,
-        edge_pen_width=edgesize,
-        edge_color=edgecolor,
-        output=f"{logger.get_snapshot_dir()}/graph.png",
-    )
-    print(f"\nGraph written to {logger.get_snapshot_dir()}/graph.png")
 
 
 def mp_to_point(
@@ -830,13 +723,13 @@ class MPEnv(ProxyEnv):
             if self.ep_step_ctr == 0 or self.ep_step_ctr == self.horizon + 1:
                 if self.learn_residual:
                     pos = action[:3] + self.get_init_target_pos()
-                    quat_delta = quat2mat(euler2mat(pos[3:6]))
-                    quat = quat_multiply(quat_delta, self._eef_xquat) #compute target orientation as q' = q_delta * q0
+                    rot_delta = euler2mat(pos[3:6])
+                    quat = mat2quat(rot_delta @ quat2mat(self._eef_xquat))
                 else:
                     if self.clamp_actions:
                         action = self.clamp_planner_action_mp_space_bounds(action)
                     pos = action
-                    quat = quat2mat(euler2mat(pos[3:6]))
+                    quat = mat2quat(euler2mat(pos[3:6]))
                 is_grasped = self.check_grasp()
                 o = mp_to_point(
                     self,
